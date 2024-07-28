b52136890b39139a9ea50256554d0d3b03edb3b5
artspessoto <artspessoto@gmail.com>
2024-07-28 00:23:50 -0300

    feat: update pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Details } from "./pages/Details";
+import { MoviePreview } from "./pages/MoviePreview";
 import GlobalStyles from "./styles/global";
 import { ThemeProvider } from "styled-components";
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <MoviePreview />
     </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/MoviePreview/index.jsx
+++ src/pages/MoviePreview/index.jsx
@@ -2,7 +2,7 @@ import { Container } from './styles';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 
-export function Details(){
+export function MoviePreview(){
     return (
         <Container>
             <Header /> 
