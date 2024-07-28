64a512d9e872fe28ca38d7c05e7386b31a0ba9f6
artspessoto <artspessoto@gmail.com>
2024-07-28 01:41:39 -0300

    feat: Update MoviePreview page and using new react-icon to ButtonText

--- src/pages/MoviePreview/index.jsx
+++ src/pages/MoviePreview/index.jsx
@@ -2,12 +2,13 @@ import { Container, Section } from "./styles";
 import { Header } from "../../components/Header";
 import { Category } from "../../components/Category";
 import { ButtonText } from "../../components/ButtonText";
+import { FiArrowLeft } from "react-icons/fi";
 
 export function MoviePreview() {
   return (
     <Container>
       <Header />
-      <ButtonText title="Voltar"/>
+      <ButtonText title="Voltar" icon={FiArrowLeft}/>
       <Section>
         <Category title="Teste" />
         <Category title="Teste2" />
