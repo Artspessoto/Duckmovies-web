7b9256c3b12824f097b5dd40dbabe6f7421ffc37
artspessoto <artspessoto@gmail.com>
2024-07-31 05:18:21 -0300

    feat: Create images folder and add test new image for login background

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Home } from "./pages/Home";
+// import { Home } from "./pages/Home";
 // import { MoviePreview } from "./pages/MoviePreview";
+import { SignIn } from "./pages/SignIn";
 import GlobalStyles from "./styles/global";
 import { ThemeProvider } from "styled-components";
 
@@ -11,7 +12,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
+      {/* <Home /> */}
       {/* <MoviePreview /> */}
     </ThemeProvider>
   </React.StrictMode>
