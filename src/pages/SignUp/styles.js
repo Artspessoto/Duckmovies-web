820f45a6c2eb5c9d162ce1e04d12cb6203cf719a
artspessoto <artspessoto@gmail.com>
2024-07-31 17:13:51 -0300

    chore: Create basic structure for SignUp page

---
+++ src/pages/SignUp/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+// import backgroundImg from "../../assets/images/cineBackground.jpg";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+`;
+
