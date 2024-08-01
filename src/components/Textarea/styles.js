5e4296d0ddc9782f23b50c192696a11809280da9
artspessoto <artspessoto@gmail.com>
2024-07-31 23:55:08 -0300

    feat: Create new component Textarea to use in CreateMovie page

---
+++ src/components/Textarea/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.textarea`
+  width: 100%;
+  height: 27rem;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAPHITE};
+  color: ${({ theme }) => theme.COLORS.LAVENDER_GRAY};
+
+  border: none;
+  resize: none;
+
+  margin-bottom: 0.8rem;
+  border-radius: 1rem;
+  padding: 1.6rem;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.COLORS.LAVENDER_GRAY};
+  }
+`;
