161645522a29061db1805ac98038a164b00fb903
artspessoto <artspessoto@gmail.com>
2024-08-01 03:36:41 -0300

    styles: add styles to MovieNoteItem component

--- src/components/MovieNoteItem/styles.js
+++ src/components/MovieNoteItem/styles.js
@@ -1,3 +1,36 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: ${({ theme, $isNew }) =>
+    $isNew ? "transparent" : theme.COLORS.GRAPHITE};
+  color: ${({ theme, $isNew }) =>
+    $isNew ? theme.COLORS.LAVENDER_GRAY : "#FFFFFF"};
+
+  border: ${({ theme, $isNew }) =>
+    $isNew ? `0.1rem dashed ${theme.COLORS.LAVENDER_GRAY}` : "none"};
+
+  margin-bottom: 0.8rem;
+  border-radius: 1rem;
+  padding-right: 1.6rem;
+
+  > button {
+    border: none;
+    background: none;
+  }
+
+  > input {
+    height: 5.6rem;
+    width: 100%;
+
+    padding: 1.2rem;
+    color: ${({theme}) => theme.COLORS.WHITE};
+    background: transparent;
+    border: none;
+    
+    &::placeholder {
+        color: ${({theme}) => theme.COLORS.LAVENDER_GRAY};
+    }
+  }
+`;
