5d8b80343500ed90fc1d7ef2afdae453507ac614
artspessoto <artspessoto@gmail.com>
2024-08-09 00:06:25 -0300

    feat(auth): Secure routes based on user auth and prevent password from being stored in LocalStorage

--- src/context/AuthContext/AuthContext.jsx
+++ src/context/AuthContext/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { api } from "../../services/api";
 import PropTypes from "prop-types";
 
@@ -6,20 +6,36 @@ export const AuthContext = createContext({});
 
 export function AuthProvider({ children }) {
   const [data, setData] = useState({});
-  
+
   async function signIn({ email, password, addAlert }) {
     try {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
 
+      const { id, password: _, ...safeUserData } = user;
+
+      localStorage.setItem("@duckmovies:user", JSON.stringify(safeUserData));
+      localStorage.setItem("@duckmovies:token", token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
-      setData({ user, token });
+
+      setData({ user: safeUserData, token });
     } catch (err) {
       const apiResponse = err.response.data.message;
       addAlert("error", apiResponse);
       return;
     }
   }
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("@duckmovies:user"));
+    const token = localStorage.getItem("@duckmovies:token");
+
+    if (user && token) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      setData({ user, token });
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
