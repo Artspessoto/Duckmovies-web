b7f4021ba812431b8029ff80fef25e195b68e2dd
artspessoto <artspessoto@gmail.com>
2024-08-14 15:25:38 -0300

    feat(movieValidation): Create schema model to use in CreateMovie

---
+++ src/validation/movieValidation.js
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const CreateMovieNotePayload = z.object({
+  title: z
+    .string()
+    .min(2, "Título (2-255)")
+    .max(255, "Título não pode exceder 255 caracteres"),
+  description: z
+    .string()
+    .max(255, "Descrição sobre filme não pode exceder 255 caracteres")
+    .optional(),
+  rating: z
+    .number()
+    .min(1, "Avaliação do filme deve ser entre 1 e 5")
+    .max("Avaliação do filme deve ser entre 1 e 5"),
+  tags: z
+    .array(z.string())
+    .min(1, "Para anotação do filme, 1 categoria é obrigatória"),
+});
