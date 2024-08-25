1ff31432bb409fcd822afea512724f525495f629
artspessoto <artspessoto@gmail.com>
2024-08-25 12:59:59 -0300

    chore: Add test in SignIn page to render form correctly

--- src/pages/__tests__/SignIn.spec.jsx
+++ src/pages/__tests__/SignIn.spec.jsx
@@ -2,12 +2,13 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import { App } from "../../containers/App";
 import { RouterWrapper } from "../../test-utils/RouterWrapper";
-import { useAuth } from "../../context/AuthContext/useAuth";
 
-export function handleSignInPage() {
-  <RouterWrapper initialEntries={["/"]}>
-    <App />
-  </RouterWrapper>;
+export function renderSignInPage() {
+  render(
+    <RouterWrapper initialEntries={["/"]}>
+      <App />
+    </RouterWrapper>
+  );
 }
 
 let mockedAddAlert = vi.fn();
@@ -29,9 +30,8 @@ vi.mock("../../context/AlertContext/useAlerts.js", () => {
 vi.mock("react-router-dom", async (importOriginal) => {
   const actual = await importOriginal();
   return {
-    actual,
+    ...actual,
     useNavigate: () => mockedNavigate,
-    BrowserRouter: actual.BrowserRouter,
   };
 });
 
@@ -41,4 +41,22 @@ vi.mock("../../context/AuthContext/useAuth.js", () => {
       signIn: mockedSignIn,
     }),
   };
+});
+
+describe("SignIn page", () => {
+  beforeEach(() => {
+    vi.resetModules();
+    vi.clearAllMocks();
+  });
+
+  it("Should be render SignIn form correctly", async () => {
+    renderSignInPage();
+
+    expect(screen.getByPlaceholderText(/E-mail/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Senha/i)).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /Entrar/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /Criar conta/i })
+    ).toBeInTheDocument();
+  });
 });
